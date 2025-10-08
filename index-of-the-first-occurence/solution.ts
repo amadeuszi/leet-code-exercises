a11772cda577db6551e160b6bdb093c20ca0a24f
Amadeusz Iwanowski <amadeusz.iwanowski@gmail.com>
2025-10-08 20:13:40 +0200

    feat: implement strStr function to find first occurrence of substring in string

---
+++ index-of-the-first-occurence/solution.ts
@@ -0,0 +1,13 @@
+function strStr(haystack: string, needle: string): number {
+    
+    for (let i = 0; i < haystack.length - needle.length + 1; i++) {
+        for (let j = 0; j < needle.length; j++) {
+            if (haystack[i + j] !== needle[j]) {
+                break;
+            } else if (j === needle.length - 1) {
+                return i
+            }
+        }
+    }
+    return -1
+};
