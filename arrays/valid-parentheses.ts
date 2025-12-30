b9854924710899336f0dcb28eabe56b0accff4fd
Amadeusz Iwanowski <amadeusz.iwanowski@gmail.com>
2025-12-30 21:04:35 +0100

    Check if parentheses are valid

---
+++ arrays/valid-parentheses.ts
@@ -0,0 +1,24 @@
+function isValid(s: string): boolean {
+    const stack = []
+    const matching = {
+        "}": "{",
+        ")": "(",
+        "]": "[",
+    }
+    const opening = new Set(["(", "{", "["])
+
+    for (let i = 0; i < s.length; i++) {
+        const current = s[i]
+        if (opening.has(current)) {
+            stack.push(current)
+        } else {
+            const match = matching[current]
+            if (match !== stack[stack.length - 1]) {
+                return false
+            }
+            stack.length = stack.length - 1
+        }
+    }
+
+    return stack.length === 0
+};
